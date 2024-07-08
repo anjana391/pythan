ba6f6b45f84e5b29c9c7ecf07e1cd9604f21f63e
anjanshakya391@gmail.com <129138155+anjana391@users.noreply.github.com>
2024-07-08 16:43:48 +0545

    update

---
+++ main.py
@@ -0,0 +1,70 @@
+# print("hello")
+# print("hello",4555,False)
+
+# name = 'anjana'
+# age = 20
+# address = 'dallu'
+
+# print(f"my name is {name} age {age} address {address}")
+
+# name = input("Enter your name: ")
+# print(name)
+
+# +,-,*,/,%,,**//
+
+# print(10%3)
+# print(4**2)
+
+# a=3
+# b=4
+# print(a is b)
+
+
+# name = 'anjana'
+# print('a' in name)
+# print('k' in name)
+
+# and,or,not
+# username = 'admin'
+# password='admin'
+
+# print(username=='admin' and password=='admin')
+
+
+
+# name = input("Enter your name : ")
+# age = input("Enter your age :")
+# address = input("Enter your address :")
+# print(name)
+# print(age)
+# print(address)
+
+a= 10
+b=5
+print(a+b)
+print(a-b)
+print(10**2)
+print(10%3)
+
+# p=float(input("Enter amount: "))
+# t = float(input("Enter a time: "))
+# r=float(input("Enter a rate: "))
+# simpleInterest = p * t * r / 100
+# print(f"Simple Interest = {simpleInterest}")
+
+# name=input("Enter student name: ")
+# english = int(input("Enter english marks: "))
+# nepali = int(input("Enter you nepali marks: "))
+
+# social = int(input("Enter you social marks: "))
+# computer = int(input("Enter you computer marks: "))
+# math = int(input("Enter you math marks: "))
+# total=english+nepali+social+computer+math
+
+# print(f"Total Marks = {total}")
+
+# # per=total/500*100
+# per=(total/500)*100
+# print(f"Total percents = {per}")
+
+
